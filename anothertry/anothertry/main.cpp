9869acab76b45f1caf973d274dd4d4061a417a51
adamWeinstein <adam.w.stein@gmail.com>
2021-08-23 11:48:08 -0600

    what am i doing

---
+++ anothertry/anothertry/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  anothertry
+//
+//  Created by Adam Weinstein on 8/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
